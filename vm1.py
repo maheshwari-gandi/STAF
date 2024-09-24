15302ce8f3c3840025881b2ab92c67392ec967ca
maheshwari-gandi <128781616+maheshwari-gandi@users.noreply.github.com>
2024-09-24 16:27:26 +0530

    Add files via upload

--- vm1.py
+++ vm1.py
@@ -1,76 +1,75 @@
 import os
 import subprocess
-
+import signal
 import pyautogui
 from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.alert import Alert
-from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.core.driver import Driver
+from pathlib import Path
+import psutil
+import subprocess
+import re
 
-chrome_options=webdriver.ChromeOptions()
-chrome_options.add_experimental_option("excludeSwitches",["enable-automation"])
-chrome_options.add_experimental_option("prefs",{"protocol_handler.excluded_schemes":{"https":False}})
+def setup(citrix_url,username,password,virtual_machine,status):
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))#,options=chrome_options))
+    driver.get(citrix_url)
+    driver.maximize_window()
+    time.sleep(8)
+    driver.find_element(By.XPATH,"//button[@id='details-button']").click()
+    driver.find_element(By.XPATH,"//a[@id='proceed-link']").click()
+    time.sleep(5)
+    print(driver.title)
+    driver.find_element(By.XPATH,"//h1[normalize-space()='Welcome to Citrix Workspace app']").is_displayed()
+    time.sleep(2)
+    driver.find_element(By.XPATH, "//a[normalize-space()='Detect Citrix Workspace app']").click()
+    time.sleep(2)
+    pyautogui.press("tab")
+    time.sleep(5)
+    pyautogui.press("enter")
+    driver.find_element(By.XPATH,"//a[@id='protocolhandler-detect-alreadyInstalledLink']").click()
+    time.sleep(60)
+    driver.find_element(By.XPATH,"//input[@id='username']").send_keys(username)
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//input[@id='password']").send_keys(password)
+    time.sleep(1)
+    driver.find_element(By.XPATH,"//a[@id='loginBtn']").click()
+    time.sleep(1)
+    driver.find_element(By.XPATH,f"//p[normalize-space()='{virtual_machine}']").click()
+    time.sleep(3)
+    driver.find_element(By.XPATH,"//div[@class='theme-highlight-color appDetails-actions-text'][normalize-space()='Open']").click()
+    time.sleep(3)
+    home_directory = Path.home()
+    print(home_directory)
+    download_dir = f"{home_directory}/Downloads"
+    files = [f for f in os.listdir(download_dir) if f.endswith('.ica')]
+    print(files)
+    paths = [os.path.join(download_dir, basename) for basename in files]
+    print(paths)
+    latest_file = max(paths, key=os.path.getctime)
+    print(latest_file)
+    process = subprocess.Popen(['open', latest_file])
+    # process=subprocess.call(('open', latest_file))
+    time.sleep(10)
+    print(process.pid)
+    # subprocess.run(['pkill','-f','pycharm'],check=True)
+    # time.sleep(10)
+    status=False
+    return status
 
+citrix_url = "https://10.91.237.221/Citrix/storeWeb/"
+username = "administrator@mypoc.local"
+password = "Test@123"
+virtual_machine = "Win2k19"
+status=True
+for i in range(1,4):
+    if True:
+        setup(citrix_url,username,password,virtual_machine,status)
+        print(f"Connected to VM {virtual_machine}")
+        break
+    else:
+        print(f"trying {i} time to connect to VM")
+else:
+    print(f"Error while connecting VM {virtual_machine}")
 
-#
-# chrome_options = Options()
-# chrome_options.add_argument('--allow-insecure-localhost')
-# chrome_options.add_argument('--ignore-certificate-errors')
-# chrome_options.add_argument('--ignore-ssl-errors')
-# chrome_options.add_argument('--disable-notifications')
-# chrome_options.add_argument('--acceptInsecureCerts')
-#
-# path_to_cookies = '~/.config/google-chrome/Default/Cookies'
-# user_data_dir='~/.config/google-chrome'
-# chrome_options.add_argument('-cookies-load-from='+path_to_cookies)
-# chrome_options.add_argument(f'--user-data-dir={user_data_dir}')
-# # Optional: Specify a profile within the user data directory
-# chrome_options.add_argument('--profile-directory=Default')
-# caps = webdriver.DesiredCapabilities.CHROME.copy()
-# caps['acceptInsecureCerts'] = True
-# driver = webdriver.Chrome(desired_capabilities=caps)
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install(),options=chrome_options))
-driver.get("https://10.91.237.221/Citrix/storeWeb/")
-driver.maximize_window()
-time.sleep(5)
-driver.find_element(By.XPATH,"//button[@id='details-button']").click()
-driver.find_element(By.XPATH,"//a[@id='proceed-link']").click()
-time.sleep(10)
-print(driver.title)
-driver.find_element(By.XPATH,"//h1[normalize-space()='Welcome to Citrix Workspace app']").is_displayed()
-time.sleep(10)
-driver.find_element(By.XPATH,"//a[normalize-space()='Detect Citrix Workspace app']").click()
-time.sleep(5)
-pyautogui.press("tab")
-time.sleep(5)
-pyautogui.press("enter")
-driver.find_element(By.XPATH,"//a[@id='protocolhandler-detect-alreadyInstalledLink']").click()
-time.sleep(3)
-driver.find_element(By.XPATH,"//input[@id='username']").send_keys("administrator@mypoc.local")
-time.sleep(3)
-driver.find_element(By.XPATH,"//input[@id='password']").send_keys("Test@123")
-time.sleep(3)
-driver.find_element(By.XPATH,"//a[@id='loginBtn']").click()
-time.sleep(3)
-ele=driver.find_element(By.XPATH,"//p[normalize-space()='Win2k19']").click()
-time.sleep(3)
-print(ele.text)
-driver.find_element(By.XPATH,"//div[@class='theme-highlight-color appDetails-actions-text'][normalize-space()='Open']").click()
-time.sleep(3)
-# driver.get("chrome://downloads/")
-# time.sleep(3)
-download_dir = "/home/sysadmin/Downloads"
-files = [f for f in os.listdir(download_dir) if f.endswith('.ica')]
-paths = [os.path.join(download_dir, basename) for basename in files]
-latest_file = max(paths, key=os.path.getctime)
-# print(latest_file)
-subprocess.call(('open', latest_file))
-time.sleep(10)
